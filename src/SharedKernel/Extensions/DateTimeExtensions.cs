1a480addc392334ec18094524a9a667e59fa8c50
Joe Ipe <joe.ipe@hotmail.com>
2025-12-24 10:16:52 +1100

    adding shared kernel

---
+++ src/SharedKernel/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace SharedKernel.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        private static readonly string dateFormat = "dd/MM/yyyy";
+
+        public static DateTime ParseDate(this string date)
+        {
+            DateTime res = DateTime.ParseExact(date, dateFormat, CultureInfo.CurrentUICulture.DateTimeFormat);
+            return res;
+        }
+
+        public static string ParseDate(this DateTime date)
+        {
+            string res = date.ToString(dateFormat);
+            return res;
+        }
+
+        public static bool IsValidDate(this string date)
+        {
+            var res = DateTime.TryParseExact(date, dateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsed);
+            return res;
+        }
+
+        public static DateTime SetKind(this DateTime DT, DateTimeKind DTKind)
+        {
+            return DateTime.SpecifyKind(DT, DTKind);
+        }
+    }
+}
---
+++ src/SharedKernel/Extensions/ObjectExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace SharedKernel.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static string OutputJson(this object item)
+        {
+            var serializeOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                //WriteIndented = true
+            };
+            var json = JsonSerializer.Serialize(item, serializeOptions);
+            return json;
+        }
+
+        public static T OutputObject<T>(this string json)
+        {
+            var serializeOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            var result = JsonSerializer.Deserialize<T>(json, serializeOptions);
+            return result;
+        }
+    }
+}
